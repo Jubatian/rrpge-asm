0640da69027b76837a8a4bdfa2518827b2da4cfa
Sandor Zsuga <jubatian@jubatian.com>
2014-05-02 14:09:29 +0200

    Updated license to RRPGE License version 2.

--- Make_config.mk
+++ Make_config.mk
@@ -4,8 +4,8 @@
 #
 # Author    Sandor Zsuga (Jubatian)
 # Copyright 2013 - 2014, GNU GPLv3 (version 3 of the GNU General Public
-#           License) extended as RRPGEv1 (version 1 of the RRPGE License): see
-#           LICENSE.GPLv3 and LICENSE.RRPGEv1 in the project root.
+#           License) extended as RRPGEv2 (version 2 of the RRPGE License): see
+#           LICENSE.GPLv3 and LICENSE.RRPGEv2 in the project root.
 #
 #
 #
--- firead.c
+++ firead.c
@@ -3,9 +3,9 @@
 **  \brief     Source file reader
 **  \author    Sandor Zsuga (Jubatian)
 **  \copyright 2013 - 2014, GNU GPLv3 (version 3 of the GNU General Public
-**             License) extended as RRPGEv1 (version 1 of the RRPGE License):
-**             see LICENSE.GPLv3 and LICENSE.RRPGEv1 in the project root.
-**  \date      2014.03.23
+**             License) extended as RRPGEv2 (version 2 of the RRPGE License):
+**             see LICENSE.GPLv3 and LICENSE.RRPGEv2 in the project root.
+**  \date      2014.05.02
 */
 
 
