e62fabe2309524c76e541df65d19845e0810f50d
Sandor Zsuga <jubatian@jubatian.com>
2014-10-15 18:27:40 +0200

    New symbol table interface working towards eventually supporting arithmetic in literals. Non-functional.

---
+++ symtab.h
@@ -0,0 +1,122 @@
+/**
+**  \file
+**  \brief     Symbol table
+**  \author    Sandor Zsuga (Jubatian)
+**  \copyright 2013 - 2014, GNU GPLv3 (version 3 of the GNU General Public
+**             License) extended as RRPGEv2 (version 2 of the RRPGE License):
+**             see LICENSE.GPLv3 and LICENSE.RRPGEv2 in the project root.
+**  \date      2014.10.14
+**
+**  Manages the symbol table: creates entries, and for pass2, resolves those.
+**  Currently singleton, but designed so it is possible to extend later.
+**
+**  Symbols in the source are identified by strings: the symbol table supports
+**  the resolution of these indexed by the strings. Internally however the
+**  table uses ID values to index symbols, which is used for implicit symbol
+**  definitions (such as parts of an expression).
+**
+**  The component is built so it may work with arbitrary (fixed) sizes,
+**  however for the singleton, these sizes are fixed and are controlled by the
+**  size definitions.
+**
+**  Section base offsets are meant to be applied using special symbols added
+**  before resolution. The suggested names for these: '$.code', '$.data',
+**  '$.head', '$.desc', '$.zero' and '$.file'.
+*/
+
+
+#ifndef SYMTAB_H
+#define SYMTAB_H
+
+
+#include "types.h"
+#include "section.h"
+#include "valwr.h"
+
+
+/* Symbol table data structure */
+typedef struct symtab_s symtab_t;
+
+
+/* Maximal number of symbol definitions. */
+#define SYMTAB_DEF_SIZE 32768U
+/* Maximal number of string symbol names. */
+#define SYMTAB_NAM_SIZE 8192U
+/* Maximal number of symbol usage entries. */
+#define SYMTAB_USE_SIZE 16384U
+/* String size for collecting symbol names. */
+#define SYMTAB_STR_SIZE (256U * 1024U)
+
+/* Symbol definition command: Source 0 is name (s0n) for other symbol flag. */
+#define SYMTAB_CMD_S0N  0x4000U
+/* Symbol definition command: Source 0 is ID (s0v) for other symbol flag. */
+#define SYMTAB_CMD_S0I  0x1000U
+/* Symbol definition command: Source 1 is name (s1n) for other symbol flag. */
+#define SYMTAB_CMD_S1N  0x8000U
+/* Symbol definition command: Source 1 is ID (s1v) for other symbol flag. */
+#define SYMTAB_CMD_S1V  0x2000U
+
+/* Symbol definition command: Add sources. */
+#define SYMTAB_CMD_ADD  0x00U
+/* Symbol definition command: Subtract: Sr0 - Sr1. */
+#define SYMTAB_CMD_SUB  0x01U
+/* Symbol definition command: Multiply sources. */
+#define SYMTAB_CMD_MUL  0x02U
+/* Symbol definition command: Divide: Sr0 / Sr1. */
+#define SYMTAB_CMD_DIV  0x03U
+/* Symbol definition command: Modulo: Sr0 % Sr1. */
+#define SYMTAB_CMD_MOD  0x04U
+/* Symbol definition command: AND: Sr0 & Sr1. */
+#define SYMTAB_CMD_AND  0x05U
+/* Symbol definition command: OR: Sr0 | Sr1. */
+#define SYMTAB_CMD_OR   0x06U
+/* Symbol definition command: XOR: Sr0 ^ Sr1. */
+#define SYMTAB_CMD_XOR  0x07U
+/* Symbol definition command: Right shift: Sr0 >> Sr1. */
+#define SYMTAB_CMD_SHR  0x08U
+/* Symbol definition command: Left shift: Sr0 << Sr1. */
+#define SYMTAB_CMD_SHL  0x09U
+
+
+
+/* Get built-in singleton object handle. */
+symtab_t* symtab_getobj(void);
+
+
+/* Initialize or resets a symbol table object (size is unchanged). The given
+** section object is bound to the symbol table. */
+void  symtab_init(symtab_t* hnd, section_t* sec);
+
+
+/* Add new symbol definition to the table. Prints fault if the symbol
+** definition can not be added. Returns ID of the definition (nonzero), or
+** zero (!) if it was not possible to add. Parameters: 'cmd': The command to
+** use for generating the value of the symbol, 's0v', 's0n': Source 0, 's1v',
+** 's1n': Source 1. The name is used only if 'cmd' requests so (then a symbol
+** usage entry is generated). */
+auint symtab_addsymdef(symtab_t* hnd, auint cmd,
+                       auint s0v, uint8 const* s0n,
+                       auint s1v, uint8 const* s1n);
+
+
+/* Add symbol name string binding to a symbol definition. Prints fault if it
+** can not be done. Returns nonzero on failure (symbol name space exhausted).
+** The name terminates with a white character, and does not need to be
+** preserved after addition. */
+auint symtab_bind(symtab_t* hnd, uint8 const* nam, auint id);
+
+
+/* Add symbol usage. The 'off' parameter supplies the offset within the
+** section where the symbol will have to be resolved. The 'use' parameter
+** gives the usage as defined in valwr.h. The name terminates with a white
+** character. The symbol does not need to be bound at this point. Returns
+** nonzero and prints fault if it is not possible to add this. */
+auint symtab_use(symtab_t* hnd, uint8 const* nam, auint off, auint use);
+
+
+/* Resolves the symbol table into the bound section. Prints fault and returns
+** nonzero if it is not possible to resolve. */
+auint symtab_resolve(symtab_t* hnd);
+
+
+#endif
