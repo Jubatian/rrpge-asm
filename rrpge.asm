d4fcaee7b8eed0564fd6cf5a78df4d41f2dc8c37
Sandor Zsuga <jubatian@jubatian.com>
2015-01-05 20:03:27 +0100

    Version 00.015.001-00: rrpge.asm updated.

--- rrpge.asm
+++ rrpge.asm
@@ -212,9 +212,17 @@ us_dsurf_getacc		equ	0xF09E
 us_dsurf_setaccpart	equ	0xF0A0
 us_dsurf_init		equ	0xF0A2
 us_dsurf_flip		equ	0xF0A4
+us_tile_set		equ	0xF0A6
+us_tile_getacc		equ	0xF0A8
+us_tile_blit		equ	0xF0AA
+us_tile_blitb		equ	0xF0AC
 
 ;
 ; User Library locations (pointers)
 ;
 
+up_font_4		equ	0xFAAC
+up_font_4i		equ	0xFAB0
+up_font_8		equ	0xFAB4
+up_font_8i		equ	0xFAB8
 up_dsurf		equ	0xFAC0
