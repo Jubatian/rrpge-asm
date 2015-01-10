d3067ac9aa9ed8de9dc6b4b08f4a4ef02b5e2743
Sandor Zsuga <jubatian@jubatian.com>
2015-01-10 19:18:19 +0100

    Version 00.015.002-00: rrpge.asm updated.

--- rrpge.asm
+++ rrpge.asm
@@ -209,13 +209,26 @@ us_dsurf_setm		equ	0xF098
 us_dsurf_setmdbuf	equ	0xF09A
 us_dsurf_get		equ	0xF09C
 us_dsurf_getacc		equ	0xF09E
-us_dsurf_setaccpart	equ	0xF0A0
-us_dsurf_init		equ	0xF0A2
-us_dsurf_flip		equ	0xF0A4
-us_tile_set		equ	0xF0A6
-us_tile_getacc		equ	0xF0A8
-us_tile_blit		equ	0xF0AA
-us_tile_blitb		equ	0xF0AC
+us_dsurf_getpw		equ	0xF0A0
+us_dsurf_setaccpart	equ	0xF0A2
+us_dsurf_init		equ	0xF0A4
+us_dsurf_flip		equ	0xF0A6
+us_tile_set		equ	0xF0A8
+us_tile_getacc		equ	0xF0AA
+us_tile_blit		equ	0xF0AC
+us_tile_blitb		equ	0xF0AE
+us_tile_gethw		equ	0xF0B0
+us_tmap_set		equ	0xF0B2
+us_tmap_setfn		equ	0xF0B4
+us_tmap_getacc		equ	0xF0B6
+us_tmap_getaccxy	equ	0xF0B8
+us_tmap_getaccxfy	equ	0xF0BA
+us_tmap_blit		equ	0xF0BC
+us_tmap_gethw		equ	0xF0BE
+us_tmap_gettilehw	equ	0xF0C0
+us_tmap_gettile		equ	0xF0C2
+us_tmap_settile		equ	0xF0C4
+us_tmap_setptr		equ	0xF0C6
 
 ;
 ; User Library locations (pointers)
