c6f4480b47e6eec7a842859714d08f941d73f6e2
Sandor Zsuga <jubatian@jubatian.com>
2015-03-11 12:25:50 +0100

    Version 00.016.000-00: Finalized.

--- version.h
+++ version.h
@@ -2,12 +2,12 @@
 **  \file
 **  \brief     The version string for the program
 **  \author    Sandor Zsuga (Jubatian)
-**  \copyright 2013 - 2014, GNU GPLv3 (version 3 of the GNU General Public
+**  \copyright 2013 - 2015, GNU GPLv3 (version 3 of the GNU General Public
 **             License) extended as RRPGEvt (temporary version of the RRPGE
 **             License): see LICENSE.GPLv3 and LICENSE.RRPGEvt in the project
 **             root.
-**  \date      2014.01.23
+**  \date      2015.03.11
 */
 
 
-#define ASSEMBLER_VERSION "00.016.000-00 (WIP)"
+#define ASSEMBLER_VERSION "00.016.000-00"
